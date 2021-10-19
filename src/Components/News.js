450bf229140a9ab8ef33c1f3cb119c768bef73e6
Simon <simongussenhoven@gmail.com>
2021-10-19 21:20:19 +0200

    Fixing bug when getting an error from fetch

--- src/Components/News.js
+++ src/Components/News.js
@@ -11,58 +11,76 @@ export default function News() {
         return date.toISOString().split("T")[0]
     }
 
-        let [isLoading, setLoading] = useState(true)
-        let [query, setQuery] = useState('Amsterdam')
-        let [news, setNews] = useState([])
-        let [fromDate, setFromDate] = useState(formatDate(lastWeek))
-        let [toDate, setToDate] = useState(formatDate(today))
+        const [isLoading, setLoading] = useState(true)
+        const [query, setQuery] = useState('Amsterdam')
+        const [news, setNews] = useState([])
+        const [fromDate, setFromDate] = useState(formatDate(lastWeek))
+        const [toDate, setToDate] = useState(formatDate(today))
+        const [sorting, setSorting] = useState("up")
 
-        const getNews = () => {    
-            setLoading(true)
-            fetch(`/.netlify/functions/politie-news?query=${query}&fromdate=${fromDate.replace(/-/g, "")}&todate=${toDate.replace(/-/g, "")}`)
-            .then((x) => x.json())
-            .then(result => setNews([...result.data.nieuwsberichten]))
-            .then(setLoading(false))
-        }
-
+        //set state when handling search
         const handleSearch = (event) => {
             event.preventDefault();
             setQuery(event.target.search.value)
         }
 
+        //set state when changing date
         const changeDate = (e, type) => {
             type === "from" ? setFromDate(e.target.value) : setToDate(e.target.value)
         }
 
+        //use function to generate items, depending on the length of the array of newsitems. Otherwise, show an error
+        const generateItems = () => {
+            if (news.length !== 0) {
+                return news.map(item => {
+                    return(
+                        <div className="card col-12 col-md-2 col-lg-2 my-1 mx-md-2 shadow d-flex flex-column" key={item.uid}>
+                            <img className="card-img-top w-100 news-image" src={item.afbeelding.url !== "" ? item.afbeelding.url : placeholder } alt={item.uid}/>
+                            <div className="card-body mb-auto">
+                                <span className="w-100"><small><strong>{item.gebied}</strong></small></span><br/>
+                                <span className="card-title"><small>{item.publicatiedatum}</small></span><br/>
+                                <span className="w-100" >{item.titel}</span> 
+                            </div>
+                            <a href={item.url} className="btn btn-primary m-2" target="_blank" rel="noreferrer">Lees meer</a>
+                        </div>
+                    )
+                })
+            }
+            else {
+                console.log("No items found!")
+                return <h2>No items found</h2>
+            }
+        }
+
+        //rerender the page when any of the mentioned state values are changed.
         useEffect(() => {
+            const getNews = () => {    
+                setLoading(true)
+                fetch(`/.netlify/functions/politie-news?query=${query}&fromdate=${fromDate.replace(/-/g, "")}&todate=${toDate.replace(/-/g, "")}`)
+                .then((x) => x.json())
+                .then(result => result.data !== undefined ? setNews([...result.data.nieuwsberichten]) : setNews([]))
+                .then(setLoading(false))
+            }
             getNews()
         },[query, fromDate, toDate]);
         
         if (!isLoading) {
             return (
                 <div className="container">
-                    <form className="d-flex justify-content-center my-3" onSubmit={handleSearch}>
+                    <form className="d-flex flex-column flex-md-row justify-content-center my-3" onSubmit={handleSearch}>
                         {isLoading ? <span>Loading</span> : ""}
-                        <input type="text" className="rounded mx-1" placeholder="Voer een zoekterm in" name="search" id="search"/>
-                        <input type="date" className="rounded mx-1" name="fromdate" max={toDate} value={fromDate} onChange={(e) => {changeDate(e, "from")}}/>
-                        <input type="date" className="rounded mx-1" name="todatedate" min={fromDate} value={toDate} onChange={(e) => {changeDate(e, "to")}}/>
-                        <input type="submit" value="Zoeken" className="mx-1"/>
+                        <input type="text" className="rounded mx-1 my-1" placeholder="Voer een zoekterm in" name="search" id="search"/>
+                        <input type="date" className="rounded mx-1 my-1" name="fromdate" max={toDate} value={fromDate} onChange={(e) => {changeDate(e, "from")}}/>
+                        <input type="date" className="rounded mx-1 my-1" name="todatedate" min={fromDate} value={toDate} max={formatDate(today)} onChange={(e) => {changeDate(e, "to")}}/>
+                        <select onChange={sortItems} className="rounded mx-1 my-1">
+                            <option value="up">Newest first</option>
+                            <option value="down">Oldest first</option>
+                        </select>
+                        <input type="submit" value="Zoeken" className="mx-1 my-1"/>
                     </form>
                     <section id="news">
                         <div className="cardgroup d-flex justify-content-center flex-row py-4 flex-wrap">
-                            {news.map(item => {
-                                return(
-                                    <div className="card col-12 col-md-2 col-lg-2 my-1 mx-md-2 shadow d-flex flex-column">
-                                        <img className="card-img-top w-100 news-image" src={item.afbeelding.url !== "" ? item.afbeelding.url : placeholder } alt={item.uid}/>
-                                        <div className="card-body mb-auto">
-                                            <span className="w-100"><small><strong>{item.gebied}</strong></small></span><br/>
-                                            <span className="card-title"><small>{item.publicatiedatum}</small></span><br/>
-                                            <span className="w-100" >{item.titel}</span> 
-                                        </div>
-                                        <a href={item.url} className="btn btn-primary m-2" target="_blank" rel="noreferrer">Lees meer</a>
-                                    </div>
-                                )
-                            })}
+                            {generateItems()}
                         </div>
                     </section>
                 </div>
